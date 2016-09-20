fc533dcf8c91cafd225cb7302f0c2aa127135552
Rachael Miller <rachael.miller05@gmail.com>
2016-09-20 18:14:47 -0400

    Update votes_controller.rb

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
   def create
     params[:votes].each do |title|
-      pitch = Pitch.find_by(:title, title)
+      pitch = Pitch.find_by(title: title)
       if pitch
         pitch.votes.create(user: current_user)
       end
