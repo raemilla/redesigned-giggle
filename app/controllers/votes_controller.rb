d27b9a9c7b698c257a6e11e9ba1ab96f0892f4e1
JNaftali <marantzj@gmail.com>
2016-09-20 18:08:40 -0400

    oops

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,10 @@
+class VotesController < ApplicationController
+  def create
+    params[:votes].each do |title|
+      pitch = Pitch.find_by(:title, title)
+      if pitch
+        pitch.votes.create(user: current_user)
+      end
+    end
+  end
+end
