e89bc951f7033c370cc8237dccdba3fcf17f1c3f
JNaftali <marantzj@gmail.com>
2016-09-20 17:12:10 -0400

    first round voting form

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :students, only: [:new, :create]
+  resources :pitches, only: [:create]
+  resources :votes, only: [:create]
+  resources :preferences, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
