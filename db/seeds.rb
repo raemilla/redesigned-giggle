964b7e652c14cb5c0a83b3aa5504df46f1f74a2b
Adam Spittler <spittler@gmail.com>
2016-09-21 10:40:52 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,21 @@ admin = Admin.find_or_create_by(full_name: "admin")
 admin.email ||= "admin@devbootcamp.com"
 admin.password ||= "password"
 admin.save
+
+students= [
+  {full_name: "Rubiksdude", email: "rubiks@dude.wow", password: "password"},
+  {full_name: "Henri", email: "henri@gmail.com", password: "password"},
+  {full_name: "fluffy", email: "fluffy@email.com", password: "password"},
+  {full_name: "pig wilson", email: "spittler@gmail.com", password: "password"}
+]
+Student.create(students)
+
+
+pitches= [
+  {title: "Where is my owner?", description: "Halp, my ownr disapear 5ever plz halp", author_id: 2},
+  {title: "What's that smell?", description: "I JUSS WANNA SMEL IT", author_id: 2},
+  {title: "Why won't the dog on the tv answer me?", description: "HEY, HEY, HEY!!! NOTICE ME! GRRRRRRRRR",author_id: 3},
+  {title: "What are zebras?", description: "R they like us but bigger?", author_id: 4},
+  {title: "wolves??", description: "r we rly descendants of these JERKS?!?!", author_id: 5}
+]
+Pitch.create(pitches)
